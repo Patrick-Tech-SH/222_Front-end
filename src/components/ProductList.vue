aad483a075c16e1da387eef2ade6b52fd09e694b
Jirasin46033 <jirasin.46033@mail.kmutt.ac.th>
2021-09-13 11:57:46 +0700

    [UPDATE] market

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,10 +1,40 @@
 <template>
   <!-- ProductList -->
   <div class="mx-9 pt-10">
-    <h1 class="text-2xl font-bold">All Games</h1>
+    <!-- <h1 class="text-2xl font-bold">All Games</h1> -->
 
     <!-- Product -->
     <div class="mt-10 grid sm:grid-cols-3 gap-3">
+      <div class="card text-center shadow-2xl">
+        <figure class="px-10 pt-10">
+          <img src="https://picsum.photos/id/1005/400/250" class="rounded-xl" />
+        </figure>
+        <div class="card-body">
+          <h1 class="card-title text-2xl">Game Name</h1>
+          <p class="text-xl">Game Dev Company</p>
+          <p class="text-xl text-yellow-500 font-bold">Price</p>
+          <p class="text-xl">Platform</p>
+          <div class="justify-center card-actions">
+            <button class="btn btn-outline btn-accent">Add to cart</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="card text-center shadow-2xl">
+        <figure class="px-10 pt-10">
+          <img src="https://picsum.photos/id/1005/400/250" class="rounded-xl" />
+        </figure>
+        <div class="card-body">
+          <h1 class="card-title text-2xl">Game Name</h1>
+          <p class="text-xl">Game Dev Company</p>
+          <p class="text-xl text-yellow-500 font-bold">Price</p>
+          <p class="text-xl">Platform</p>
+          <div class="justify-center card-actions">
+            <button class="btn btn-outline btn-accent">Add to cart</button>
+          </div>
+        </div>
+      </div>
+
       <div class="card text-center shadow-2xl">
         <figure class="px-10 pt-10">
           <img src="https://picsum.photos/id/1005/400/250" class="rounded-xl" />
--- src/views/Market.vue
+++ src/views/Market.vue
@@ -1,13 +1,40 @@
 <template>
-<div>
-    Market
-</div>
+    <div>
+        <div class="px-2 mx-8 mt-16 text-2xl font-bold mb-10">
+            <p>Market</p>
+        </div>
+
+        <!-- menu bar -->
+        <div>
+            <div class="flex flex-row mx-8">
+                <a class="my-1 text-sm md:text-lg font-medium text-gray-700 dark:text-gray-200 hover:text-indigo-500 dark:hover:text-indigo-400 mx-1 sm:mx-4 lg:my-0"
+                    href="#">All Games</a>
+                <a class="my-1 text-sm md:text-lg font-medium text-gray-700 dark:text-gray-200 hover:text-indigo-500 dark:hover:text-indigo-400 mx-1 sm:mx-4 lg:my-0"
+                    href="#">Nintendo</a>
+                <a class="my-1 text-sm md:text-lg font-medium text-gray-700 dark:text-gray-200 hover:text-indigo-500 dark:hover:text-indigo-400 mx-1 sm:mx-4 lg:my-0"
+                    href="#">Sony</a>
+                <a class="my-1 text-sm md:text-lg font-medium text-gray-700 dark:text-gray-200 hover:text-indigo-500 dark:hover:text-indigo-400 mx-1 sm:mx-4 lg:my-0"
+                    href="#">Ubisoft</a>
+                <a class="my-1 text-sm md:text-lg font-medium text-gray-700 dark:text-gray-200 hover:text-indigo-500 dark:hover:text-indigo-400 mx-1 sm:mx-4 lg:my-0"
+                    href="#">Comcap</a>
+            </div>
+        </div>
+
+        <div>
+            <product-list />
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-    name: "Market"
-}
+import ProductList from "../components/ProductList.vue"
+
+    export default {
+        name: "Market",
+        components:{
+            ProductList
+        }
+    }
 </script>
 
 <style>
