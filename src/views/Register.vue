b12b1bf6fee8174cfadb9389aa4427b886b86f3c
Jirasin46033 <jirasin.46033@mail.kmutt.ac.th>
2021-09-13 10:46:34 +0700

    [UPDATE] Register

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,14 +3,20 @@
     <div class="md:px-5 border px-5 py-3 xl:py-8 m-3 rounded-md md:mx-32 xl:mx-96 shadow-md xl:my-48 my-28">
       <h1 class="mb-10 text-4xl text-center">Register</h1>
       <div class="flex flex-col mb-6 gap-3 md:px-10 lg:px-48">
-        <label class="">Email</label>
-        <input type="text" class="border p-2">
+        <label class="">Username</label>
+        <input type="text" class="border p-2" placeholder="Username">
         <label>Password</label>
-        <input type="text" class="border p-2">
+        <input type="text" class="border p-2" placeholder="Password">
+        <label>Email</label>
+        <input type="text" class="border p-2" placeholder="Email">
+        <label>First name</label>
+        <input type="text" class="border p-2" placeholder="First name">
+        <label>Last name</label>
+        <input type="text" class="border p-2" placeholder="Lastname">
       </div>
 
       <div class="text-center">
-        <button class="btn btn-active mx-auto">Login</button>
+        <button class="btn btn-active mx-auto">Sign Up</button>
       </div>
     </div>
   </div>
