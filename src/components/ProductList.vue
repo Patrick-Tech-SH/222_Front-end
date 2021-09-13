42fdb63dcee11b9937e4959d4fe0a558a22f0c34
Jirasin46033 <jirasin.46033@mail.kmutt.ac.th>
2021-09-13 15:41:13 +0700

    [FIX] productlist

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -5,37 +5,58 @@
 
     <!-- Product -->
     <div class="mt-10 grid sm:grid-cols-3 gap-3">
-      <div class="card text-center shadow-2xl">
-        <figure class="px-10 pt-10">
-          <img src="https://picsum.photos/id/1005/400/250" class="rounded-xl" />
-        </figure>
-        <div class="card-body">
-          <h1 class="card-title text-2xl">Game Name</h1>
-          <p class="text-xl">Game Dev Company</p>
-          <p class="text-xl text-yellow-500 font-bold">Price</p>
-          <p class="text-xl">Platform</p>
-          <div class="justify-center card-actions">
-            <button class="btn btn-outline btn-accent">Add to cart</button>
+      <div>
+        <div class="card bordered">
+          <figure>
+            <img src="https://picsum.photos/id/1005/400/250">
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">Game Name</h2>
+            <p class="">Game Dev Company Name</p>
+            <p class="text-yellow-600">Price</p>
+            <p class="">Platform</p>
+            <div class="justify-end card-actions">
+              <button class="btn btn-accent">Add to cart</button>
+            </div>
           </div>
         </div>
       </div>
 
-      <div class="card text-center shadow-2xl">
-        <figure class="px-10 pt-10">
-          <img src="https://picsum.photos/id/1005/400/250" class="rounded-xl" />
-        </figure>
-        <div class="card-body">
-          <h1 class="card-title text-2xl">Game Name</h1>
-          <p class="text-xl">Game Dev Company</p>
-          <p class="text-xl text-yellow-500 font-bold">Price</p>
-          <p class="text-xl">Platform</p>
-          <div class="justify-center card-actions">
-            <button class="btn btn-outline btn-accent">Add to cart</button>
+      <div>
+        <div class="card bordered">
+          <figure>
+            <img src="https://picsum.photos/id/1005/400/250">
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">Game Name</h2>
+            <p class="">Game Dev Company Name</p>
+            <p class="text-yellow-600">Price</p>
+            <p class="">Platform</p>
+            <div class="justify-end card-actions">
+              <button class="btn btn-accent">Add to cart</button>
+            </div>
           </div>
         </div>
       </div>
 
-      <div class="card text-center shadow-2xl">
+      <div>
+        <div class="card bordered">
+          <figure>
+            <img src="https://picsum.photos/id/1005/400/250">
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">Game Name</h2>
+            <p class="">Game Dev Company Name</p>
+            <p class="text-yellow-600">Price</p>
+            <p class="">Platform</p>
+            <div class="justify-end card-actions">
+              <button class="btn btn-accent">Add to cart</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- <div class="card text-center shadow-2xl">
         <figure class="px-10 pt-10">
           <img src="https://picsum.photos/id/1005/400/250" class="rounded-xl" />
         </figure>
@@ -48,7 +69,8 @@
             <button class="btn btn-outline btn-accent">Add to cart</button>
           </div>
         </div>
-      </div>
+      </div> -->
+
     </div>
   </div>
 </template>
