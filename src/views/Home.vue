b8e1dedf75d63b5cbe7f4bb3df7c7bb00073890d
nirawat <nirawat@opsta.co.th>
2021-12-03 15:38:54 +0700

    remove nonuse

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,6 @@
   <div>
     <introduction />
     <game-dev-list />
-    <div class="mx-9 pt-10">
-      <h1 class="text-2xl font-bold">Example Game</h1>
-    </div>
     <!-- <product-list /> -->
     <div class="text-center pt-8">
         <router-link to="/market" class="btn btn-outline">View All Products</router-link>
